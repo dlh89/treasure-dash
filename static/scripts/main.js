37ca3cdeb2a4c198fbb82171215927ec44b7fb8b
David Hide <d.hide@live.co.uk>
2019-05-25 16:22:14 +0100

    Listen for click on cells, send ajax post request and modify info text based on response

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,8 +1,34 @@
-$.ajax({
-  method: "POST",
-  url: "/result",
-  data: { row: 2, col: 3 }
-})
-.done(function( msg ) {
-  alert( "Data Saved: " + msg );
-});
+var cells = $('.grid__cell');
+$(cells).on('click', cellClick);
+
+function cellClick(e) {
+  var row = jQuery(e.target).data('row');
+  var col = jQuery(e.target).data('col');
+  var infoText = jQuery('.info__text');
+  
+  $.ajax({
+    method: "POST",
+    url: "/result",
+    data: { row: row, col: col }
+  })
+  .done(function( msg ) {
+    var infoResult = '';
+    switch (msg) {
+      case 'success':
+        infoResult = 'Congratulations!';
+        break;
+      case 'cold':
+        infoResult = 'Ice cold.';
+        break;
+      case 'warm':
+        infoResult = 'Getting warm.';
+        break;
+      case 'hot':
+        infoResult = 'Red hot!';
+        break;
+    }
+    jQuery(infoText).text(infoResult);
+    var infoTextModifierClass = 'info__text--' + msg;
+    jQuery(infoText).addClass(infoTextModifierClass);
+  });
+}

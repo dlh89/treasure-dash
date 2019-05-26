72592fb665480486547ec9e28b415b8714425548
David Hide <d.hide@live.co.uk>
2019-05-26 14:20:12 +0100

    Send dig event between server and clients

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
 from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send, emit
+from pprint import pprint
 from game import game
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+# @app.route("/")
+# def index():
+#     return render_template('index.html')
 
-@app.route("/result", methods=['POST', 'GET'])
-def result():
-    if request.method == 'POST':
-        row = request.form['row']
-        col = request.form['col']
-        result = game(row, col)
-        return result
+# @app.route("/result", methods=['POST', 'GET'])
+# def result():
+#     if request.method == 'POST':
+#         row = request.form['row']
+#         col = request.form['col']
+#         result = game(row, col)
+#         return result
+
+online_users = []
+
+@socketio.on("connect")
+def handle_connect():
+    print("on connect")
+    sid = request.sid
+    online_users.append(sid)
+    emit("user_connected", {"user": sid})
+
+@socketio.on("dig")
+def handle_json(json):
+    result = game(json['row'], json['col'])
+    dig_data = {'row': json['row'], 'col': json['col'], 'result': result}
+    emit('dig', dig_data, broadcast=True)
+
+if __name__ == "__main__":
+    socketio.run(app)
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,42 +1,48 @@
-var cells = $('.grid__cell');
-$(cells).on('click', cellClick);
+$(document).ready(function() {
+  var socket = io.connect('http://127.0.0.1:5000');
 
-function cellClick(e) {
-  var row = jQuery(e.target).data('row');
-  var col = jQuery(e.target).data('col');
-  var infoText = jQuery('.info__text');
-  
-  $.ajax({
-    method: "POST",
-    url: "/result",
-    data: { row: row, col: col }
-  })
-  .done(function( msg ) {
-    var infoResult = '';
-    var gridClass = 'grid__cell--dug';
-    switch (msg) {
-      case 'success':
-        infoResult = 'Congratulations!';
-        gridClass = 'grid__cell--treasure';
-        break;
-      case 'cold':
-        infoResult = 'Ice cold.';
-        break;
-      case 'warm':
-        infoResult = 'Getting warm.';
-        break;
-      case 'hot':
-        infoResult = 'Red hot!';
-        break;
-    }
-    var infoTextModifierClass = 'info__text info__text--' + msg;
-    jQuery(infoText).text(infoResult)
-                    .finish()
-                    .removeClass()
-                    .addClass(infoTextModifierClass)
-                    .fadeIn(1000)
-                    .delay(2000)
-                    .fadeOut(1000);
-    jQuery(e.target).addClass(gridClass);
+  socket.on('user_connected', function(request) {
+    console.log(request);
   });
-}
+
+  socket.on('dig', function(digData) {
+    msg = digData['result'];
+    var infoText = jQuery('.info__text');
+    var infoResult = '';
+      var gridClass = 'grid__cell--dug';
+      switch (msg) {
+        case 'success':
+          infoResult = 'Congratulations!';
+          gridClass = 'grid__cell--treasure';
+          break;
+        case 'cold':
+          infoResult = 'Ice cold.';
+          break;
+        case 'warm':
+          infoResult = 'Getting warm.';
+          break;
+        case 'hot':
+          infoResult = 'Red hot!';
+          break;
+      }
+      var infoTextModifierClass = 'info__text info__text--' + msg;
+      jQuery(infoText).text(infoResult)
+                      .finish()
+                      .removeClass()
+                      .addClass(infoTextModifierClass)
+                      .fadeIn(1000)
+                      .delay(2000)
+                      .fadeOut(1000);
+      jQuery('[data-row=' + digData['row'] + '][data-col=' + digData['col'] + ']').addClass(gridClass);
+  });
+
+  var cells = $('.grid__cell');
+  $(cells).on('click', cellClick);
+  
+  function cellClick(e) {
+    var row = jQuery(e.target).data('row');
+    var col = jQuery(e.target).data('col');
+    
+    socket.emit('dig', {row: row, col: col});
+  }
+})
